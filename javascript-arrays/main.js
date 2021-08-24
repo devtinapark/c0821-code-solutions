080bd8c0e5fd95850868255483f1441b1428e210
Christine Tina Park <escp1004jk@gmail.com>
2021-08-23 17:34:48 -0700

    Completed javascript-arrays. - Christine Tina Park

---
+++ javascript-arrays/main.js
@@ -0,0 +1,25 @@
+var colors = [
+  'red',
+  'white',
+  'blue'
+];
+console.log('value of colors[0]: ', colors[0]);
+console.log('value of colors[1]: ', colors[1]);
+console.log('value of colors[2]: ', colors[2]);
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.');
+colors[2] = 'green';
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.');
+console.log('"colors" array: ', colors);
+
+var students = [
+  'Tina',
+  'Verity',
+  'Kevin',
+  'Jerry'
+];
+var numberOfStudents = students.length;
+console.log('There are ' + numberOfStudents + ' students in the class.');
+var lastIndex = numberOfStudents - 1;
+var lastStudent = students[lastIndex];
+console.log('The last student in the array is ' + lastStudent + '.');
+console.log('"students" array: ', students);
