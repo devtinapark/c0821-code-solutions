840e5551bced5884b2ce867b005898afa8bdceed
Christine Tina Park <escp1004jk@gmail.com>
2021-08-24 15:51:04 -0700

    Completed javascript-functions. - Christine Tina Park

--- javascript-functions/main.js
+++ javascript-functions/main.js
@@ -20,14 +20,14 @@ function getArea(width, height) {
 console.log('getArea(5,25): ', getArea(5, 25));
 
 function getFirstName(person) {
-  person = {};
-  return person.firstName;
+  var firstName = person.firstName;
+  return firstName;
 }
 
 console.log("getFirstName({firstName: 'Christine', lastName: 'Park'}): ", getFirstName({ firstName: 'Christine', lastName: 'Park' }));
 
 function getLastElement(array) {
-  array = [];
-  return array[array.length - 1];
+  var lastElement = array[array.length - 1];
+  return lastElement;
 }
-console.log("getLastElement(['propane', 'and', 'propane', 'accessories'])", getLastElement(['propane', 'and', 'propane', 'accessories']));
+console.log("getLastElement(['propane', 'and', 'propane', 'accessories']): ", getLastElement(['propane', 'and', 'propane', 'accessories']));
