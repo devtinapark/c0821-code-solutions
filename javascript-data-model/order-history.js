af2402e8f45d5d79a96e1fb8d814373f26f54da5
Christine Tina Park <escp1004jk@gmail.com>
2021-08-24 12:52:24 -0700

    Fixed consistency of properties.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -6,17 +6,22 @@ var orderHistory = [
       endON: 8772232
     },
     orderPlaced: new Date(2020, 8, 4),
-    returnCLose: new Date(2020, 9, 7),
     delivered: new Date(2020, 8, 8),
     shipTo: 'JS Masher',
-    pictureUrl: null,
-    title: 'JavaScript for impatient programmers',
-    author: {
-      authorLastName: 'Rauschmayer',
-      authorFirstName: 'Dr. Axel'
-    },
-    subtotal: 31.55,
-    total: 34.00
+    total: 34.00,
+    itemDetails: [
+      {
+        type: 'book',
+        pictureUrl: null,
+        title: 'JavaScript for impatient programmers',
+        author: {
+          authorLastName: 'Rauschmayer',
+          authorFirstName: 'Dr. Axel'
+        },
+        returnCLose: new Date(2020, 9, 7),
+        subtotal: 31.55
+      }
+    ]
   },
   {
     orderNumber: {
@@ -25,17 +30,22 @@ var orderHistory = [
       endON: 1280257
     },
     orderPlaced: new Date(2020, 7, 19),
-    returnCLose: new Date(2020, 8, 19),
     delivered: new Date(2020, 7, 20),
     shipTo: 'JS Masher',
-    pictureUrl: null,
-    title: 'The Timeless Way of Building',
-    author: {
-      authorLastName: 'Alexander',
-      authorFirstName: 'Christopher'
-    },
-    subtotal: 41.33,
-    total: 44.53
+    total: 44.53,
+    itemDetails: [
+      {
+        type: 'book',
+        pictureUrl: null,
+        title: 'The Timeless Way of Building',
+        author: {
+          authorLastName: 'Alexander',
+          authorFirstName: 'Christopher'
+        },
+        returnCLose: new Date(2020, 8, 19),
+        subtotal: 41.33
+      }
+    ]
   },
   {
     orderNumber: {
@@ -44,17 +54,18 @@ var orderHistory = [
       endON: 9059409
     },
     orderPlaced: new Date(2020, 7, 4),
-    returnCLose: new Date(2020, 8, 5),
     delivered: new Date(2020, 7, 7),
     shipTo: 'JS Masher',
-    pictureUrl: null,
-    title: 'Gamecube Controller Adapter',
-    author: {
-      authorLastName: null,
-      authorFirstName: null
-    },
-    subtotal: 15.98,
-    total: 17.22
+    total: 17.22,
+    itemDetails: [
+      {
+        type: 'gameHardware',
+        pictureUrl: null,
+        title: 'Gamecube Controller Adapter',
+        returnCLose: new Date(2020, 8, 5),
+        subtotal: 15.98
+      }
+    ]
   },
   {
     orderNumber: {
@@ -63,28 +74,28 @@ var orderHistory = [
       endON: 2648248
     },
     orderPlaced: new Date(2020, 7, 3),
-    returnCLose: new Date(2020, 8, 4),
     delivered: new Date(2020, 7, 5),
     shipTo: 'JS Masher',
-    pictureUrl: [
-      null,
-      null
-    ],
-    title: [
-      'GameCube Controller',
-      'The Art of Sql'
-    ],
-    author: {
-      authorLastName: [
-        null,
-        'Faroult'
-      ],
-      authorFirstName: [
-        null,
-        'Stephane'
-      ]
-    },
-    subtotal: [94.95, 33.99],
-    total: 138.93
+    total: 138.93,
+    itemDetails: [
+      {
+        type: 'gameHardware',
+        pictureUrl: null,
+        title: 'GameCube Controller',
+        returnCLose: new Date(2020, 8, 4),
+        subtotal: 94.95
+      },
+      {
+        type: 'book',
+        pictureUrl: null,
+        title: 'The Art of Sql',
+        author: {
+          authorLastName: 'Faroult',
+          authorFirstName: 'Stephane'
+        },
+        returnCLose: new Date(2020, 8, 4),
+        subtotal: 33.99
+      }
+    ]
   }
 ];
