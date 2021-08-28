3e54e09defc6f86659a89a7c8d1f51cfc28e0755
Christine Tina Park <escp1004jk@gmail.com>
2021-08-27 18:20:14 -0700

    Fixed unnecessary conditionals.

--- array-manipulation-1/src/initial.js
+++ array-manipulation-1/src/initial.js
@@ -1,10 +1,8 @@
 /* exported initial */
 function initial(array) {
   var newArray = [];
-  for (var i = 0; i < array.length; i++) {
-    if (i !== array.length - 1) {
-      newArray.push(array[i]);
-    }
+  for (var i = 0; i < array.length - 1; i++) {
+    newArray.push(array[i]);
   }
   return newArray;
 }
--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -1,10 +1,8 @@
 /* exported tail */
 function tail(array) {
   var newArray = [];
-  for (var i = 0; i < array.length; i++) {
-    if (i !== 0) {
-      newArray.push(array[i]);
-    }
+  for (var i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
   }
   return newArray;
 }
