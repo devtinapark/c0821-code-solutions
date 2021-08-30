e1f57bb48db54659f2f99a29355e5f5f57592c5d
Christine Tina Park <escp1004jk@gmail.com>
2021-08-30 13:38:36 -0700

    Completed string-manipulation-2. - Christine Tina Park

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,1 +1,8 @@
 /* exported firstChars */
+function firstChars(length, string) {
+  var newString = '';
+  for (var i = 0; i < length && i < string.length; i++) {
+    newString += string[i];
+  }
+  return newString;
+}
--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,12 @@
 /* exported lastChars */
+function lastChars(length, string) {
+  var newString = '';
+  if (length > string.length) {
+    newString += string;
+  } else {
+    for (var i = string.length - length; i < string.length; i++) {
+      newString += string[i];
+    }
+  }
+  return newString;
+}
