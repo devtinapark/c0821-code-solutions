cc126c92bab1ffe1993f3988c136588db2139159
Christine Tina Park <escp1004jk@gmail.com>
2021-08-30 12:24:20 -0700

    RD

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -3,9 +3,9 @@ function ransomCase(string) {
   var newWord = '';
   for (var i = 0; i < string.length; i++) {
     if (i % 2 === 0) {
-      newWord += string[i].toUppercase();
-    } else {
       newWord += string[i].toLowerCase();
+    } else {
+      newWord += string[i].toUpperCase();
     }
   }
   return newWord;
