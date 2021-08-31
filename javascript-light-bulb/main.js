0bf9787d94f213473f0777ed266cfd67d5804713
Christine Tina Park <escp1004jk@gmail.com>
2021-08-31 11:40:25 -0700

    Simplified condition statement using boolean.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,14 +1,17 @@
 var $currentBG = document.querySelector('#currentBG');
 var $currentCircle = document.querySelector('#currentCircle');
+var isLightOn = true;
 
 function doSwitch(event) {
-  if ($currentBG.className === 'lightBG') {
+  if (isLightOn === true) {
     $currentBG.className = 'darkBG';
     $currentCircle.className = 'circle darkCircle';
+    isLightOn = false;
   } else {
     $currentBG.className = 'lightBG';
     $currentCircle.className = 'circle lightCircle';
+    isLightOn = true;
   }
 }
 
-$currentCircle.addEventListener('click', doSwitch, false);
+$currentCircle.addEventListener('click', doSwitch);
