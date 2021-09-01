8be3ebe5b83505170267d2292e8639253efa8dc1
Christine Tina Park <escp1004jk@gmail.com>
2021-09-01 14:52:24 -0700

    RD

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,14 +1,36 @@
 var $char = document.querySelectorAll('span');
 var i = 0;
+var success = 0;
+var fail = 0;
+var $success = document.querySelector('.success');
+var $fail = document.querySelector('.fail');
+var $accuracy = document.querySelector('.accuracy');
 
 function check(event) {
-  if (event.key === $char.item(i).textContent) {
-    $char.item(i).className = 'passed';
-    $char.item(i + 1).className = 'underlined';
+  if (event.key === $char[i].textContent) {
+    $char[i].className = 'passed';
+    $char[i + 1].className = 'underlined';
+    success += 1;
     i += 1;
   } else {
-    $char.item(i).className = 'error';
+    $char[i].className = 'error';
+    fail += 1;
   }
 }
 
+function showAccuracy(event) {
+  if (i === $char.length - 1) {
+    $success.innerHTML = success;
+    $fail.innerHTML = fail;
+    var accuracy = (success / (success + fail) * 100).toFixed(2);
+    $accuracy.innerHTML = accuracy;
+  }
+}
+
+function askAgain(event) {
+
+}
+
 window.addEventListener('keydown', check);
+window.addEventListener('keydown', showAccuracy);
+window.addEventListener('keydown', askAgain);
