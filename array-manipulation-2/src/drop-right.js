10000630a568ce35d4f99b8d1c5564c2de0e9aa3
Christine Tina Park <escp1004jk@gmail.com>
2021-09-08 13:10:15 -0700

    Return empty array

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,12 +1,12 @@
 /* exported dropRight */
 function dropRight(array, count) {
   var newArray = [];
-  if (count > array.length) {
-    newArray = array;
-  } else {
+  if (count <= array.length) {
     for (var j = 0; j < (array.length - count); j++) {
       newArray.push(array[j]);
     }
+    return newArray;
+  } else {
+    return [];
   }
-  return newArray;
 }
