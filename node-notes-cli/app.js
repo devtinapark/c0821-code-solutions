39069ed8dd47a663c63495a2edc5f562e579382d
Christine Tina Park <escp1004jk@gmail.com>
2021-10-07 18:12:58 +0000

    Completed node-notes-cli. - Christine Tina Park

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,12 +1,57 @@
 const fs = require('fs');
 
-function read() {
+if (process.argv[2] === 'read') {
+  read();
+} else if (process.argv[2] === 'create') {
+  create();
+} else if (process.argv[2] === 'delete') {
+  del();
+} else if (process.argv[2] === 'update') {
+  update();
+}
+
+function create() {
   fs.readFile('data.json', 'utf8', (err, data) => {
     if (err) throw err;
-    console.log(data);
+    const dataParsed = JSON.parse(data);
+    dataParsed.notes[dataParsed.nextID.toString()] = process.argv[3];
+    dataParsed.nextID++;
+    const dataJSON = JSON.stringify(dataParsed, null, 2);
+    fs.writeFile('data.json', dataJSON, err => {
+      if (err) throw err;
+    });
   });
 }
 
-if (process.argv[2] === 'read') {
-  read();
+function read() {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const dataParsed = JSON.parse(data);
+    const dataNotesJSON = JSON.stringify(dataParsed.notes, null, 2);
+    console.log(dataNotesJSON);
+  });
+}
+
+function del() {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const dataParsed = JSON.parse(data);
+    delete dataParsed.notes[process.argv[3]];
+    const dataJSON = JSON.stringify(dataParsed, null, 2);
+    fs.writeFile('data.json', dataJSON, err => {
+      if (err) throw err;
+    });
+  });
+}
+
+function update() {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const dataParsed = JSON.parse(data);
+    dataParsed.notes[process.argv[3]] = process.argv[4];
+    const dataJSON = JSON.stringify(dataParsed);
+    fs.writeFile('data.json', dataJSON, err => {
+      if (err) throw err;
+    });
+  });
 }
---
+++ node-notes-cli/test.js
@@ -0,0 +1,7 @@
+console.log(process.argv0);
+console.log(process.argv[0]);
+console.log(process.argv[1]);
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+console.log(process.argv[4]);
+console.log(process.argv[5]);
