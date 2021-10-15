4e095528e007c9e2799556a7de29a20df164e299
Christine Tina Park <escp1004jk@gmail.com>
2021-10-15 18:33:02 +0000

    added filter function.

---
+++ array-filter/filter.js
@@ -0,0 +1,12 @@
+function filter(array, predicate) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const inputElement = array[i];
+    if (predicate(inputElement)) {
+      newArray.push(inputElement);
+    }
+  }
+  return newArray;
+}
+
+const evenP = number => number % 2 === 0;
