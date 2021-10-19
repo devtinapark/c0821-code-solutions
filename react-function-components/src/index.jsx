be3c7e72835f4682e673db090111a5647a37073e
Christine Tina Park <escp1004jk@gmail.com>
2021-10-19 19:26:37 +0000

    Completed react-function-components. - Christine Tina Park

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const CustomButton = <button>Click Me!</button>;
+const div = document.querySelector('#root');
+
+ReactDOM.render(
+  CustomButton,
+  div
+);
