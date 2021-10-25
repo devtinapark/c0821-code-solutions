a1cda841b42abfe3ae32db923cbd2b9e1f4892f1
Christine Tina Park <escp1004jk@gmail.com>
2021-10-25 22:31:59 +0000

    Completed fetch-in-react. - Tina

---
+++ fetch-in-react/src/app.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import UserList from './user-list';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: false
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then(data => {
+        const arrayUsers = [];
+        for (let i = 0; i < 5; i++) {
+          arrayUsers.push(data[i]);
+        }
+        this.setState({ users: arrayUsers });
+      })
+      .catch(console.error);
+  }
+
+  render() {
+    return this.state.isLoading
+      ? <p>Loading...</p>
+      : <UserList users={this.state.users} />;
+  }
+}
+
+export default App;
