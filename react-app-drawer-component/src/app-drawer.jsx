b8c95a3c42f6ed158ac82e214eec036eb42b25f3
Christine Tina Park <escp1004jk@gmail.com>
2021-10-26 06:59:08 +0000

    Completed react-app-drawer-component. - Tina

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -2,9 +2,38 @@ import React from 'react';
 export default class AppDrawer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { open: true };
+    this.doOpen = this.doOpen.bind(this);
+    this.doClose = this.doClose.bind(this);
+  }
+
+  doOpen() {
+    this.setState({ open: true });
+  }
+
+  doClose() {
+    this.setState({ open: false });
   }
 
   render() {
+    return (
+        <>
+          <i
+            id="drawer-button"
+            className="fas fa-bars fa-2x"
+            onClick={ this.doOpen }></i>
+          <div
+          className={ this.state.open ? 'shaded' : 'shaded hidden' }
+          onClick={ this.doClose }></div>
+          <div className={ this.state.open ? 'menu-container' : 'menu-container closed' }>
+            <div className="menu-contents">
+              <button className="menu-header">Menu</button>
+              <button className="link" onClick={this.doClose}>About</button>
+              <button className="link" onClick={this.doClose}>Get Started</button>
+              <button className="link" onClick={this.doClose}>Sign In</button>
+            </div>
+          </div>
+        </>
+    );
   }
 }
