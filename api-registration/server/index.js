93a4bc0f49178017b7bc7f8356c2cdd44dfd3e55
Christine Tina Park <escp1004jk@gmail.com>
2021-10-27 00:45:53 +0000

    Hided password

--- api-registration/server/index.js
+++ api-registration/server/index.js
@@ -29,7 +29,7 @@ app.post('/api/auth/sign-up', (req, res, next) => {
       const sql = `
         insert into "users" ("username", "hashedPassword")
         values ($1, $2)
-        returning *`;
+        returning "createdAt", "userId", "username"`;
       const params = [username, hashedPassword];
       db.query(sql, params)
         .then(result => {
