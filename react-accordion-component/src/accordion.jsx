7c44147169e66bd33d3ef312cf9c351aece3a384
Christine Tina Park <escp1004jk@gmail.com>
2021-10-27 00:06:19 -0700

    RD

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,21 +1,103 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import data from './data';
 
-export default class Accordion extends React.Component {
+class ContentRow extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      bodyClass: ''
+    };
+    this.setBodyClass = this.setBodyClass.bind(this);
+  }
+
+  setBodyClass() {
+    const isOpen = this.props.isOpen;
+    if (isOpen === true) {
+      this.setState = { bodyClass: 'topicBody' };
+    } else {
+      this.setState = { bodyClass: 'topicBody closed' };
+    }
   }
 
   render() {
-    for (let i = 0; i < data.length; i++) {
+    const head = this.props.head;
+    const body = this.props.body;
+    const bodyClass = this.state.bodyClass;
+    return (
+      <>
+        <div className="topicHead">
+          <h3>
+            {head}
+          </h3>
+        </div>
+        <div className={bodyClass}>
+          <p>
+            {body}
+          </p>
+        </div>
+      </>
+    );
+  }
+}
 
+class AccordionContents extends React.Component {
+  render() {
+    const idOpen = this.props.idOpen;
+    const isOpen = this.props.isOpen;
+    const rows = [];
+    if (isOpen === false) {
+      this.props.topics.forEach(topic => {
+        rows.push(
+          <ContentRow
+            isOpen={false}
+            head={topic.head}
+            body={topic.body} />
+        );
+      });
+    } else {
+      this.props.topics.forEach(topic => {
+        if (topic.id === idOpen) {
+          rows.push(
+            <ContentRow
+              isOpen={true}
+              head={topic.head}
+              body={topic.body} />
+          );
+        } else {
+          rows.push(
+            <ContentRow
+              isOpen={false}
+              head={topic.head}
+              body={topic.body} />
+          );
+        }
+      });
     }
     return (
+      { rows }
+    );
+  }
+}
+
+class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      idOpen: 1,
+      isOpen: false
+    };
+  }
+
+  render() {
+    return (
       <div className="continer">
-        <TopicHead />
-        <TopicBody />
+        <AccordionContents
+          topics={ this.props.topics }
+          idOpen={ this.state.idOpen }
+          isOpen={ this.state.isOpen }
+        />
       </div>
     );
   }
 }
+
+export default Accordion;
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordion from './accordion';
 
+const TOPICS = [
+  {
+    id: 1,
+    head: 'How to Succeed in Software Engineering',
+    body: 'Just keep coding'
+  },
+  {
+    id: 2,
+    head: 'How to Have fun in Life',
+    body: 'Believe in yourself and strive for a balance in life'
+  },
+  {
+    id: 3,
+    head: 'How to be Healhy',
+    body: 'Take care of your diet, workout, and sleep habits'
+  }
+];
+
 ReactDOM.render(
-  <Accordion />,
+  console.log(TOPICS);
+  <Accordion topics={TOPICS} />,
   document.getElementById('root')
 );
