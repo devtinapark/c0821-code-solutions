8e322026e485fa3a1794abb3ddafcf89b55af625
Christine Tina Park <escp1004jk@gmail.com>
2021-10-28 18:00:04 +0000

    Modified a attribute

--- react-hash-routing/client/components/header.jsx
+++ react-hash-routing/client/components/header.jsx
@@ -6,8 +6,7 @@ export default function Header(props) {
       <nav className="navbar navbar-dark bg-dark shadow-sm">
         <div className="container">
           <div className="col px-0">
-            {/* this anchor should go back to the catalog at '#' */}
-            <a href="" className="navbar-brand">
+            <a href="#" className="navbar-brand">
               <i className="fa fa-dollar-sign" /> Wicked Sales
             </a>
           </div>
