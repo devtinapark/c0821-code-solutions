85e950160ebfa3c67f1e090c1050a105288b052b
Christine Tina Park <escp1004jk@gmail.com>
2021-10-29 18:44:22 +0000

    Optimized codes for defaults function

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,12 +1,6 @@
 /* exported defaults */
 
 function defaults(target, source) {
-  for (var key in target) {
-    if (typeof key === 'undefined') {
-      target = source;
-    }
-  }
-
   for (var key2 in source) {
     if (typeof target[key2] === 'undefined') {
       target[key2] = source[key2];
