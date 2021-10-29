952518f59cb0716083b3a23834ec72f8b7bc122c
Christine Tina Park <escp1004jk@gmail.com>
2021-10-29 18:22:18 +0000

    Completed object-manipulation-2. - Christine Tina Park

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,13 +1,15 @@
 /* exported defaults */
 
 function defaults(target, source) {
+  for (var key in target) {
+    if (typeof key === 'undefined') {
+      target = source;
+    }
+  }
+
   for (var key2 in source) {
     if (typeof target[key2] === 'undefined') {
       target[key2] = source[key2];
-    } else {
-      if (!Object.keys(target).includes(key2)) {
-        target[key2] = source[key2];
-      }
     }
   }
 }
