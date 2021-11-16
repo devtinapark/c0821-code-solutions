d9bd5016c1fa0dc0f5171101d12acd7c342f8d53
Christine Tina Park <escp1004jk@gmail.com>
2021-11-16 00:56:29 +0000

    Changed Big O Notations

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -27,13 +27,13 @@ function uniqueQuadratic(words) {
     const word = words[i];          // 2 * n = O(2n)
     let isUnique = true;            // 1 * 1 = O(1)
     for (
-      let c = 0;                    // 1 * 1 = O(1)
-      c < i;                        // 1 * n = O(n)
-      c++                           // 2 * n = O(2n)
+      let c = 0;                    // 1 * n * n = O(n^2)
+      c < i;                        // 1 * n * n = O(n^2)
+      c++                           // 2 * n * n = O(2n^2)
     ) {
-      const comparing = words[c];   // 2 * n = O(2n)
-      if (comparing === word) {     // 1 * 1 = O(1)
-        isUnique = false;           // 1 * 1 = O(1)
+      const comparing = words[c];   // 2 * n * n = O(2n^2)
+      if (comparing === word) {     // 1 * n * n = O(1n^2)
+        isUnique = false;           // 1 * n * n = O(1n^2)
       }
     }
     if (isUnique) {                 // 1 * 1 = O(1)
@@ -41,4 +41,4 @@ function uniqueQuadratic(words) {
     }
   }
   return unique;                    // 1 * 1 = O(1)
-} // Big O Notation for uniqueQuadratic: O(n)
+} // Big O Notation for uniqueQuadratic: O(n^2)
