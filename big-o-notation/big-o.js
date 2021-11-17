d4c2c85f537d9863214910ee453e7ba953fbdc38
Christine Tina Park <escp1004jk@gmail.com>
2021-11-17 01:16:57 +0000

    Final modification

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -5,13 +5,13 @@ function uniqueLinear(words) {
   const unique = [];                // 1 * 1 = O(1)
   for (
     let i = 0;                      // 1 * 1 = O(1)
-    i < words.length;               // 2 * n = O(2n)
-    i++                             // 2 * n = O(2n)
+    i < words.length;               // 2 * n = O(n)
+    i++                             // 2 * n = O(n)
   ) {
-    const word = words[i];          // 2 * n = O(2n)
-    if (!seen[word]) {              // 2 * 1 = O(2n)
+    const word = words[i];          // 2 * n = O(n)
+    if (!seen[word]) {              // 3 * n = O(n)
       seen[word] = true;
-      unique[unique.length] = word; // 3 * 1 = O(3)
+      unique[unique.length] = word; // 3 * n = O(n)
     }
   }
   return unique;
@@ -21,23 +21,23 @@ function uniqueQuadratic(words) {
   const unique = [];                // 1 * 1 = O(1)
   for (
     let i = 0;                      // 1 * 1 = O(1)
-    i < words.length;               // 2 * n = O(2n)
-    i++                             // 2 * n = O(2n)
+    i < words.length;               // 2 * n = O(n)
+    i++                             // 2 * n = O(n)
   ) {
-    const word = words[i];          // 2 * n = O(2n)
-    let isUnique = true;            // 1 * 1 = O(1)
+    const word = words[i];          // 2 * n = O(n)
+    let isUnique = true;            // 1 * n = O(n)
     for (
-      let c = 0;                    // 1 * n * n = O(n^2)
+      let c = 0;                    // 1 * n = O(n)
       c < i;                        // 1 * n * n = O(n^2)
-      c++                           // 2 * n * n = O(2n^2)
+      c++                           // 2 * n * n = O(n^2)
     ) {
-      const comparing = words[c];   // 2 * n * n = O(2n^2)
-      if (comparing === word) {     // 1 * n * n = O(1n^2)
-        isUnique = false;           // 1 * n * n = O(1n^2)
+      const comparing = words[c];   // 2 * n * n = O(n^2)
+      if (comparing === word) {     // 1 * n * n = O(n^2)
+        isUnique = false;           // 1 * n * n = O(n^2)
       }
     }
-    if (isUnique) {                 // 1 * 1 = O(1)
-      unique[unique.length] = word; // 3 * 1 = O(3)
+    if (isUnique) {                 // 1 * n = O(n)
+      unique[unique.length] = word; // 3 * n = O(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
